7b595252418fadd5ffe417962f9f9bfddb191339
Neph <tony3322@naver.com>
2022-01-03 09:30:43 +0900

    [Chore] swift lint 적용, add snapkit

--- SeSacFarm/SeSacFarm/SceneDelegate.swift
+++ SeSacFarm/SeSacFarm/SceneDelegate.swift
@@ -8,9 +8,7 @@
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
     var window: UIWindow?
-
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
@@ -34,7 +32,4 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func sceneDidEnterBackground(_ scene: UIScene) {
     }
-
-
 }
-
