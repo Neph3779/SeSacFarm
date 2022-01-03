fe4a3ef79a39549b740c4a1b2ee2b2c4538483b9
Neph <tony3322@naver.com>
2022-01-03 09:26:42 +0900

    First Commit

---
+++ SeSacFarm/SeSacFarm/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  SeSacFarm
+//
+//  Created by 천수현 on 2022/01/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
