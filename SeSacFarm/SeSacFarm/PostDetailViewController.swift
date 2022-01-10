4b16bc777c83ca55472e93b341b35c2e1f247c81
Neph <tony3322@naver.com>
2022-01-10 14:22:29 +0900

    [Feat] 키보드 높이에 따라 댓글창 높이 up down 해결

--- SeSacFarm/SeSacFarm/PostDetailViewController.swift
+++ SeSacFarm/SeSacFarm/PostDetailViewController.swift
@@ -32,6 +32,8 @@ final class PostDetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(backgroundTap))
+        view.addGestureRecognizer(tapGestureRecognizer)
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)),
                                                name: UIResponder.keyboardWillShowNotification, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)),
@@ -76,7 +78,6 @@ final class PostDetailViewController: UIViewController {
             backgroudnView.bottom.equalTo(replyView).inset(5)
         }
 
-        replyTextField.delegate = self
         replyTextField.backgroundColor = .clear
         replyTextField.placeholder = "댓글을 입력해주세요"
         replyBackgroudnView.addSubview(replyTextField)
@@ -87,6 +88,7 @@ final class PostDetailViewController: UIViewController {
 
     private func bindTableView() {
         postDetailViewModel.comments
+            .observe(on: MainScheduler.instance)
             .bind(to: tableView.rx.items(
                 cellIdentifier: "tableViewCell",
                 cellType: UITableViewCell.self)
@@ -97,24 +99,39 @@ final class PostDetailViewController: UIViewController {
                 cell.contentConfiguration = content
             }
             .disposed(by: disposeBag)
+
+        replyTextField.rx.controlEvent(.editingDidEndOnExit)
+            .subscribe { _ in
+                self.postDetailViewModel.returnKeyTapped.onNext(())
+                self.postDetailViewModel.replyText.onNext(self.replyTextField.text!)
+                self.replyTextField.text?.removeAll()
+            }
+            .disposed(by: disposeBag)
     }
 
-    // FIXME: 가끔 view가 safearea 밑까지 내려가버림
     @objc func keyboardWillShow(_ notification: NSNotification) {
         if let keyboardFrame: NSValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue {
             let keyboardRectangle = keyboardFrame.cgRectValue
             let keyboardHeight = keyboardRectangle.height
-            print(keyboardHeight, view.frame.origin.y)
-            view.frame.origin.y -= keyboardHeight
+            let bottomSafeAreaHeight = view.frame.height
+            - (view.safeAreaLayoutGuide.layoutFrame.origin.y
+            + view.safeAreaLayoutGuide.layoutFrame.height)
+
+            replyView.snp.remakeConstraints { replyView in
+                replyView.leading.trailing.equalTo(view.safeAreaLayoutGuide)
+                replyView.bottom.equalTo(view.safeAreaLayoutGuide).inset(keyboardHeight - bottomSafeAreaHeight)
+            }
         }
     }
 
     @objc func keyboardWillHide(_ notification: NSNotification) {
-        if let keyboardFrame: NSValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue {
-            let keyboardRectangle = keyboardFrame.cgRectValue
-            let keyboardHeight = keyboardRectangle.height
-            view.frame.origin.y += keyboardHeight
+        replyView.snp.remakeConstraints { replyView in
+            replyView.leading.trailing.bottom.equalTo(view.safeAreaLayoutGuide)
         }
+    }
+
+    @objc func backgroundTap(_ sender: UITapGestureRecognizer) {
+        self.view.endEditing(true)
     }
 }
 
@@ -125,7 +142,9 @@ extension PostDetailViewController: UITableViewDelegate {
                 as? PostDetailHeaderView else {
                     return UIView()
                 }
-        postDetailViewModel.post.subscribe(onNext: { post in
+        postDetailViewModel.post
+            .observe(on: MainScheduler.instance)
+            .subscribe(onNext: { post in
             headerView.setValues(userName: post.user.userName, date: post.createdDate,
                                  description: post.text, replyCount: post.comments.count)
         }).disposed(by: disposeBag)
@@ -134,9 +153,9 @@ extension PostDetailViewController: UITableViewDelegate {
     }
 }
 
-extension PostDetailViewController: UITextFieldDelegate {
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        view.endEditing(true)
-        return true
-    }
-}
+// TODO: 댓글 서버에 올리고 view refresh 해주기
+// TODO: 로그인 실패시 뷰 전환 안하고 토스트만 띄워주기
+// TODO: 이것저것 시도하다 token 만료된 경우 navigation의 rootView로 보내기 + 토스트 띄우기 (로그인 시작화면)
+// TODO: 유저 id 보관하기, 수정 시도시 내가 안쓴건 수정 메뉴 안띄우기
+// TODO: 글 작성해서 서버에 올리고 refresh 해주기
+// TODO: 페이지네이션 구현하기
